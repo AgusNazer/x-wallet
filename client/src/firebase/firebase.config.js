69bdbb550c774bde264a8d504cf0a93207e23785
agusNazer <agustin.nazer@hotmail.com>
2025-01-28 01:36:19 -0300

    add 2 mode env domain: dev/prodcution for deploy

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -14,7 +14,9 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
    apiKey: import.meta.env.VITE_FIREBASE_API_KEY, // Usa import.meta.env, NO process.env
-   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+   authDomain: import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_DEV
+    : import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_PROD,
    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
